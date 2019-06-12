8bbae9ab551f26c022b63548032dada879927bdb
leyla li <leylali@qq.com>
2019-06-12 16:39:58 -0700

    deleted extra things and fixed math problem to round the total cookies numbers

--- js/app.js
+++ js/app.js
@@ -1,33 +1,25 @@
 'use strict';
 
-//firstLocation
 var time=['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM',];
-
-
+//firstLocation
 
 var firstLocation = {
   name: '1st and Pike',
   minGuest: 23,
   maxGuest: 65,
   avgCookies: 6.3,
-  cookiesNum:[],
   totalCookies: 0,
   addCookies: function (){
     var cookiesElement = document.getElementById('firstPlace');
-    console.log(cookiesElement);
-    var cookiesNum2 = [];
+    var cookiesSold = [];
     for(var i = 0; i<time.length;i++){
-      var newCookie = Math.floor(Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies;
-      cookiesNum2[i]=newCookie;
-      console.log(cookiesNum2);
+      var newCookie = Math.floor((Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies);
+      cookiesSold[i]=newCookie;
       var newItem = document.createElement('li');
-      newItem.textContent = `${time[i]}: ${cookiesNum2[i]} cookies`;
-      console.log(newItem);
+      newItem.textContent = `${time[i]}: ${cookiesSold[i]} cookies`;
       cookiesElement.appendChild(newItem);
-      this.totalCookies += Math.floor(cookiesNum2[i]);
-      
+      this.totalCookies += Math.floor(cookiesSold[i]);
     }
-    this.cookiesNum = cookiesNum2;
     var newItem2 = document.createElement('li');
     newItem2.textContent = `Total: ${this.totalCookies} cookies`;
     cookiesElement.appendChild(newItem2);
@@ -36,77 +28,50 @@ var firstLocation = {
 
 };
 firstLocation.addCookies();
-console.log(firstLocation.cookiesNum);
 
 //secondLocation
-
-var time2=['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM',];
-
-
-
 var secondLocation = {
   name: 'SeaTac Airport',
   minGuest: 3,
   maxGuest: 24,
   avgCookies: 1.2,
-  cookiesNum:[],
   totalCookies: 0,
   addCookies: function (){
     var cookiesElement = document.getElementById('secondPlace');
-    console.log(cookiesElement);
-    var cookiesNum2 = [];
+    var cookiesSold = [];
     for(var i = 0; i<time.length;i++){
-      var newCookie = Math.floor(Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies;
-      cookiesNum2[i]=newCookie;
-      console.log(cookiesNum2);
+      var newCookie = Math.floor((Math.random()*(this.maxGuest-this.minGuest) + this.minGuest)* this.avgCookies) ;
+      cookiesSold[i]=newCookie;
       var newItem = document.createElement('li');
-      newItem.textContent = `${time[i]}: ${cookiesNum2[i]} cookies`;
-      console.log(newItem);
+      newItem.textContent = `${time[i]}: ${cookiesSold[i]} cookies`;
       cookiesElement.appendChild(newItem);
-      this.totalCookies += Math.floor(cookiesNum2[i]);
-      
+      this.totalCookies += Math.floor(cookiesSold[i]);
     }
-    this.cookiesNum = cookiesNum2;
     var newItem2 = document.createElement('li');
     newItem2.textContent = `Total: ${this.totalCookies} cookies`;
     cookiesElement.appendChild(newItem2);
-
   }
-
 };
 secondLocation.addCookies();
-console.log(secondLocation.cookiesNum);
-
 
 //thirdLocation
-
-var time3=['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM',];
-
-
-
 var thirdLocation = {
   name: 'Seattle Center',
   minGuest: 11,
   maxGuest: 38,
   avgCookies: 3.7,
-  cookiesNum:[],
   totalCookies: 0,
   addCookies: function (){
     var cookiesElement = document.getElementById('thirdPlace');
-    console.log(cookiesElement);
-    var cookiesNum2 = [];
+    var cookiesSold = [];
     for(var i = 0; i<time.length;i++){
-      var newCookie = Math.floor(Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies;
-      cookiesNum2[i]=newCookie;
-      console.log(cookiesNum2);
+      var newCookie = Math.floor((Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies);
+      cookiesSold[i]=newCookie;
       var newItem = document.createElement('li');
-      newItem.textContent = `${time[i]}: ${cookiesNum2[i]} cookies`;
-      console.log(newItem);
+      newItem.textContent = `${time[i]}: ${cookiesSold[i]} cookies`;
       cookiesElement.appendChild(newItem);
-      this.totalCookies += Math.floor(cookiesNum2[i]);
-      
+      this.totalCookies += Math.floor(cookiesSold[i]);
     }
-    this.cookiesNum = cookiesNum2;
     var newItem2 = document.createElement('li');
     newItem2.textContent = `Total: ${this.totalCookies} cookies`;
     cookiesElement.appendChild(newItem2);
@@ -115,39 +80,25 @@ var thirdLocation = {
 
 };
 thirdLocation.addCookies();
-console.log(thirdLocation.cookiesNum);
-
-
 
 //fourthLocation
-
-var time4=['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM',];
-
-
-
 var fourthLocation = {
   name: 'Capitol Hill',
   minGuest: 20,
   maxGuest: 38,
   avgCookies: 2.3,
-  cookiesNum:[],
   totalCookies: 0,
   addCookies: function (){
     var cookiesElement = document.getElementById('fourthPlace');
-    console.log(cookiesElement);
-    var cookiesNum2 = [];
+    var cookiesSold = [];
     for(var i = 0; i<time.length;i++){
-      var newCookie = Math.floor(Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies;
-      cookiesNum2[i]=newCookie;
-      console.log(cookiesNum2);
+      var newCookie = Math.floor((Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies);
+      cookiesSold[i]=newCookie;
       var newItem = document.createElement('li');
-      newItem.textContent = `${time[i]}: ${cookiesNum2[i]} cookies`;
-      console.log(newItem);
+      newItem.textContent = `${time[i]}: ${cookiesSold[i]} cookies`;
       cookiesElement.appendChild(newItem);
-      this.totalCookies += Math.floor(cookiesNum2[i]);
-      
+      this.totalCookies += Math.floor(cookiesSold[i]);
     }
-    this.cookiesNum = cookiesNum2;
     var newItem2 = document.createElement('li');
     newItem2.textContent = `Total: ${this.totalCookies} cookies`;
     cookiesElement.appendChild(newItem2);
@@ -156,37 +107,26 @@ var fourthLocation = {
 
 };
 fourthLocation.addCookies();
-console.log(fourthLocation.cookiesNum);
 
 //fifthLocation
-
-var time5=['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM',];
-
-
-
 var fifthLocation = {
   name: 'Alki',
   minGuest: 2,
   maxGuest: 16,
   avgCookies: 4.6,
-  cookiesNum:[],
   totalCookies: 0,
   addCookies: function (){
     var cookiesElement = document.getElementById('fifthPlace');
-    console.log(cookiesElement);
-    var cookiesNum2 = [];
+    var cookiesSold = [];
     for(var i = 0; i<time.length;i++){
-      var newCookie = Math.floor(Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies;
-      cookiesNum2[i]=newCookie;
-      console.log(cookiesNum2);
+      var newCookie = Math.floor((Math.random()*(this.maxGuest-this.minGuest) + this.minGuest) * this.avgCookies);
+      cookiesSold[i]=newCookie;
       var newItem = document.createElement('li');
-      newItem.textContent = `${time[i]}: ${cookiesNum2[i]} cookies`;
-      console.log(newItem);
+      newItem.textContent = `${time[i]}: ${cookiesSold[i]} cookies`;
       cookiesElement.appendChild(newItem);
-      this.totalCookies += Math.floor(cookiesNum2[i]);
-      
+      this.totalCookies += Math.floor(cookiesSold[i]);
+
     }
-    this.cookiesNum = cookiesNum2;
     var newItem2 = document.createElement('li');
     newItem2.textContent = `Total: ${this.totalCookies} cookies`;
     cookiesElement.appendChild(newItem2);
@@ -195,5 +135,5 @@ var fifthLocation = {
 
 };
 fifthLocation.addCookies();
-console.log(fifthLocation.cookiesNum);
+
 
